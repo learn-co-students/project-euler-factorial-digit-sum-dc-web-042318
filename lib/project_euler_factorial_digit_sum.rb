c84fad98c86b9a8c1f184f7a3a3a6b8b135d8a8f
eckmLJE <lucas.eckman@gmail.com>
2018-08-01 16:36:07 -0400

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,22 @@
 # code your solution here
+
+def factorial(n)
+    num = n
+    product = 1
+    while num > 0
+        product = product * (num)
+        num -= 1
+    end
+    product
+end
+
+def sum_of_digits(n)
+    str_arr = n.to_s.split('')
+    int_arr = str_arr.map {|el| el.to_i}
+    int_arr.reduce(0, :+)
+end
+
+def factorial_digit_sum(n)
+    product = factorial(n)
+    sum_of_digits(product)
+end
